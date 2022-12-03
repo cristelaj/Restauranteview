c5349c2044db132b687d8ef64d56ae7bb5dacd4e
Cristela Juarez <119710393+cristelaj@users.noreply.github.com>
2022-12-02 20:06:52 -0600

    Add files via upload

---
+++ DomicilioProduct.js
@@ -0,0 +1,53 @@
+const products = [
+  {
+    id: 0,
+    name: "drink",
+    price: 29.99,
+    instock: 100,
+    description:
+      "",
+    imgSrc: "./ImgLogos/drinks.jpg",
+  },
+  {
+    id: 1,
+    name:"salad",
+    price: 24.99,
+    instock: 43,
+    description:
+      "",
+    imgSrc: "./ImgLogos/salad.jpg",
+  },
+  {
+    id: 2,
+    name: "drink",
+    price: 19.99,
+    instock: 10,
+    description:
+      "",
+    imgSrc: "./ImgLogos/drinks.jpg",
+  },
+  {
+    id: 3,
+    name: "salad",
+    price: 25.99,
+    description:
+      "",
+    imgSrc: "./ImgLogos/salad.jpg",
+  },
+  {
+    id: 4,
+    name: "drink",
+    price: 29.99,
+    description:
+      "",
+    imgSrc: "./ImgLogos/drinks.jpg",
+  },
+  {
+    id: 5,
+    name: "salad",
+    price: 39.99,
+    description:
+      "",
+    imgSrc: "./ImgLogos/salad.jpg",
+  },
+];
